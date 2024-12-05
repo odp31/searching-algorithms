b3611800f45fc2d384fb69db2ac83a9f3a9de871
Olivia Polos <38380599+odp31@users.noreply.github.com>
2024-12-05 14:47:02 -0500

    Create interpolationSearch.c

---
+++ c/interpolationSearch.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int interpolationSearch(int arr[], int lo, int hi, int x) {
+  if(lo <= hi && x >= arr[lo] && x <= arr[hi]) {
+    int pos = lo + (((hi - lo) / (arr[hi] - arr[lo])) * (x - arr[lo]));
+    if(arr[pos] == x)
+      return pos;
+    if(arr[pos] < x)
+      return interpolationSearch(arr, pos + 1, hi, x);
+    if(arr[pos] > x)
+      return interpolationSearch(arr, lo, pos - 1, x);
+  }
+  return -1;
+}
+
+int main() {
+  int arr[] = {10, 12, 13, 16, 18, 19, 20, 21, 22, 23, 24, 33, 35, 42}
+  int n = sizeof(arr) / sizeof(arr[0]);
+  int x = 20;
+  int index = interpolationSearch(arr, 0, n - 1, x);
+
+  if(index != -1)
+    printf("index of %d is %d\n", x, index);
+  else
+    printf("%d isn't present in array\n", x);
+  return 0;
+}
